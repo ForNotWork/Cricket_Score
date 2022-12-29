3f5b5a176b8239349b64f49e654d3d9eb99368c0
ForNotWork <120727683+ForNotWork@users.noreply.github.com>
2022-12-29 13:51:56 +0530

    Update Cricket_Game.c

--- Cricket_Game.c
+++ Cricket_Game.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int win()
+double Win_Status(int AI_SCORE,int PLAYER_SCORE,int REMAINING_PLAYERS,int Balls)
 {
-    int AI_Score,player_Score,Remaining_Players;
-    if(AI_Score<player_Score)
+    int Over=Balls;
+    int AI_Score = AI_SCORE,player_Score=PLAYER_SCORE,Remaining_Players=REMAINING_PLAYERS;
+    if(AI_Score< player_Score)
     {
         return 1;
     }
@@ -12,27 +13,36 @@ int win()
     {
         return 0;
     }
+    else if(Over>6)
+    {
+        return 0;
+    }
     else
         return -1;
 }
-int AI_Score_Board(int RunI,int WicketI)
+void AI_Score_Board(int RunI,int WicketI)
 {
-    int Score_Run,Score_Wicket;
-    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd\n");
-    printf("/xcd/xcd/xcd SCORE BOARD xcd/xcd/xcd/xcd/xcd\n");
-    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd");
-    printf("\n");
-    printf("RUN:%d\t\t",Score_Run);
-    printf("WICKET:%d\t\n",Score_Wicket);
+    int Score_Run=RunI,Score_Wicket=WicketI;
+    printf("\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
+    printf("\xcd\xcd\xcd SCORE BOARD \xcd\xcd\xcd\xcd\xcd\n");
+    printf("\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd");
+    printf("______________________\n");
+    printf("|RUN:   |%d\t\t",Score_Run);
+    printf("|WICKET:|%d\t\n",Score_Wicket);
+    printf("______________________");
 }
-int Player_Score_Board(int RunP,int WicketP)
+void Player_Score_Board(int Run_P,int Balls,int wICKETS,int Ai_SCORE)
 {
-    int Score_Run,Score_Wicket,Remaining_Runs;
-    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd\n");
-    printf("/xcd/xcd/xcd SCORE BOARD xcd/xcd/xcd/xcd/xcd\n");
-    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd");
+    int over=Balls;
+    int Score_RunP = Run_P;
+    int Score_Wicket = wICKETS;
+    int Remaining_Runs =Ai_SCORE-Score_RunP; 
+    printf("\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
+    printf("\xcd\xcd\xcd SCORE BOARD xcd\xcd\xcd\xcd\xcd\n");
+    printf("\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd");
     printf("\n");
-    printf("RUN:%d\t\t",Score_Run);
+    printf("RUN:%d\t\t",Score_RunP);
+    printf("Balls:%d",over);
     printf("WICKET:%d\t\n",Score_Wicket);
     printf("REMAINING RUNS:%d",Remaining_Runs);
 }
@@ -49,10 +59,11 @@ int Wicket()
     int out;
     time_t t1;
     srand((unsigned)time(&t1));
-    out = rand()%4;
+    out = rand()%6;
     return out;
 }
 
+void Result(int P_Score,int Com_Score,int P_WIcket,int Com_Wicket,int over);
 int main()
 {
     int AI_Wicket[10]={1,5,3,7,4};
@@ -60,14 +71,18 @@ int main()
     time_t t,t3;
     int TURN=0;
     int Runs[10]={0,1,2,4,6},PLAYER_SCORE=0;
-    char out[20]={"RunOut","BOLD","Catch"},Player_Status;
+    char out[500]={'R','B','L','C','H'};
+    char *Player_Status;
     int PR,PO,hit;
-    int RI,OI,win=-1,Remaining_Player=4;
+    double win=-1;
+    int RI,OI,Remaining_Player=4;
     srand((unsigned)time(&t));
     RI = rand()%10;
     OI = rand()%6;
-    AI_Score_Board(AI_SCORE[RI],AI_Wicket[OI]);
-    do
+    int Com_Score=AI_SCORE[RI];
+    int Com_Wicket=AI_Wicket[OI];
+    AI_Score_Board(Com_Score,Com_Wicket);
+   do
     {
         //hit = getch();
         srand((unsigned)time(&t3));
@@ -76,6 +91,7 @@ int main()
         {
             PO = Wicket();
             Player_Status = out[PO];
+            printf("%s",Player_Status);
             Remaining_Player-=1;
         }
         else
@@ -84,12 +100,27 @@ int main()
             int RUNS= Runs[PR];
             PLAYER_SCORE += RUNS;
         }
-        TURN++;
-        Player_Score_Board(PLAYER_SCORE,TURN,Remaining_Player);
-        Win_Status(AI_SCORE,PLAYER_SCORE,Remaining_Player,TURN);
-    } while (win = -1);
-    
-    Player_Score_Board();
-    
+        ++TURN;
+        Player_Score_Board(PLAYER_SCORE,TURN,Remaining_Player,Com_Score);
+        win = Win_Status(Com_Score,PLAYER_SCORE,Remaining_Player,TURN);
+    } while (win == -1);
+    Result(int P_Score,int Com_Score,int P_WIcket,int Com_Wicket,int over);     
+    //Player_Score_Board(PLAYER_SCORE,TURN,Remaining_Player,);
     return 0;
+}
+
+void Result(int P_Score,int Com_Score,int P_WIcket,int Com_Wicket,int over)
+{
+    if((p_score >Com_Score) && (over <= 6))
+    {
+        printf("PLAYER WON!!");
+    }
+    else if(P_WIcket>4)
+    {
+        printf("PLAYER LOAST!!");
+    }
+    else
+    {
+        printf("PLAYER LOAST!!");
+    }
 }
